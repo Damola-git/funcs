d31626e755207746d922766ba7938468992840b6
Adejumo Adedamola <damoladavidadejumo@gmail.com>
2024-10-06 23:47:43 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function add7(number){
+    return number + 7;
+}
+console.log(add7(50));//*output is 57
+
+function multiply(a,b){
+    return a*b;
+}
+console.log(multiply(10,10)); //*output is 100
+
+function Capitalise(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowercase()
+
+}
+console.log(Capitalise(hEllO lOve))
